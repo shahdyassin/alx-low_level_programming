f60f03a0303c15db5ab4f481a6f5ff177d7b1ac8
shahdyassin <shahdyassin63@gmail.com>
2023-03-21 16:46:17 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,39 @@
 #include "main.h"
 
-/**
- * print_to_98 - print n to 98 counts
- * separated by comma, followed
- * by space and number should be
- * printed in order
- *
- * @n: input
- */
-void print_to_98(int n)
+#include <stdio.h>
+/*
+     *S*/void print_to_98(int n)
 {
-if (n < 98)
-{
-for (n = 0; n <= 98; n++)
-{
-_putchar(n);
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
-else if (n == 98)
-{
-_putchar(n);
-_putchar('\n');
-}
-else
-{
-for (n = 0; n > 98; n--)
-{
-_putchar(n);
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
 }
