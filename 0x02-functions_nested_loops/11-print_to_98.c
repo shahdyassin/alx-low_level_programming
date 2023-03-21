a52dbe9d5505f85933d8d5f0a0c2bffbf885af3a
shahdyassin <shahdyassin63@gmail.com>
2023-03-21 13:25:53 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
+ * print_to_98 - print n to 98 counts
  * separated by comma, followed
  * by space and number should be
  * printed in order
