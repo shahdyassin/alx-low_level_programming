8e6168780e612b3cce268d8592978f2c614c6974
shahdyassin <shahdyassin63@gmail.com>
2023-03-23 05:26:56 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return: Always 0
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0); }
